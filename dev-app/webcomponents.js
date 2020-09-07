911899719c160b2b68cbf2c9aed1ec5f6e6080f8
Tomas Kulhanek <tomaton@centrum.cz>
2020-09-07 17:18:59 +0100

    REFACTOR rename without bdl

--- dev-app/webcomponents.js
+++ dev-app/webcomponents.js
@@ -18,44 +18,44 @@ export function configure(aurelia) {
   aurelia.start().then(() => aurelia.setRoot());
    */
   aurelia.use
-    .basicConfiguration()
-    //.standardConfiguration()
-    .plugin('aurelia-bootstrapper')
-    .plugin('aurelia-history-browser')
-    .plugin('aurelia-templating-resources')
-    .plugin('aurelia-templating-router')
-    .plugin('aurelia-dynamic-html')
-    .feature('resources/index');
+    //.basicConfiguration()
+    .standardConfiguration()
+    //.plugin('aurelia-bootstrapper')
+    //.plugin('aurelia-history-browser')
+    //.plugin('aurelia-templating-resources')
+    //.plugin('aurelia-templating-router')
+    //.plugin('aurelia-dynamic-html')
+    .feature('resources/index')
     //use this routine to register component as web component
-    /*.globalResources([
-      './elements/bdl-range.html',
-      './elements/bdl-receptacle.html',
-      './elements/bdl-bind2previous',
-      './elements/bdl-dygraphchart',
-      './elements/bdl-beaker.html',
-      './elements/bdl-value',
-      './elements/bdl-beakercontrols.html',
-      './elements/bdl-markdown',
-      './elements/bdl-markdownnav',
-      './elements/bdl-simplegif',
-      './elements/bdl-capillary.html',
-      './elements/bdl-ecg',
-      './elements/bdl-chartjs',
-      './elements/bdl-chartjs-time',
-      './elements/bdl-chartjs-xy',
-      './elements/bdl-cardiaccycle.html',
-      './elements/bdl-animate-gif',
-      './elements/bdl-animate-sync-gif',
-      './elements/bdl-animate-control',
-      './elements/bdl-quiz',
-      './elements/bdl-audio-on-increase',
-      './elements/bdl-audio-on-decrease',
-      './elements/bdl-sound-on-increase',
-      './elements/bdl-markdown-book',
-      './elements/bdl-markdown-book-au',
-      './elements/bdl-markdown-app.html',
-      './elements/bdl-fmi'
-    ])*/
+    .globalResources([
+    //  './elements/bdl-range.html',
+    //  './elements/bdl-receptacle.html',
+    //  './elements/bdl-bind2previous',
+    //  './elements/bdl-dygraphchart',
+    //  './elements/bdl-beaker.html',
+    //  './elements/bdl-value',
+    //  './elements/bdl-beakercontrols.html',
+    //  './elements/bdl-markdown',
+    //  './elements/bdl-markdownnav',
+    //  './elements/bdl-simplegif',
+    //  './elements/bdl-capillary.html',
+    //  './elements/bdl-ecg',
+    //  './elements/bdl-chartjs',
+    //  './elements/bdl-chartjs-time',
+    //  './elements/bdl-chartjs-xy',
+    //  './elements/bdl-cardiaccycle.html',
+    //  './elements/bdl-animate-gif',
+    //  './elements/bdl-animate-sync-gif',
+    //  './elements/bdl-animate-control',
+    //  './elements/bdl-quiz',
+    //  './elements/bdl-audio-on-increase',
+    //  './elements/bdl-audio-on-decrease',
+    //  './elements/bdl-sound-on-increase',
+//      './elements/bdl-markdown-book',
+//      './elements/bdl-markdown-book-au',
+//      './elements/bdl-markdown-app.html',
+    //  './elements/bdl-fmi'
+    ]);
   aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
   if (environment.testing) {
     aurelia.use.plugin('aurelia-testing');
@@ -64,6 +64,8 @@ export function configure(aurelia) {
   aurelia.start().then(() => {
     const registry = aurelia.container.get(CustomElementRegistry);
     registry.fallbackPrefix = 'bdl-';
+    registry.mandatoryPrefix = true;
+    //registry.register(BdlMarkdownBook);
     registry.useGlobalElements();
   });
 }
---
+++ src/elements/trigger-on-increase.js
@@ -0,0 +1,45 @@
+import {Value} from './value';
+import {bindable} from 'aurelia-templating';
+export class TriggerOnIncrease extends Value {
+  @bindable thresholdvalue;
+  constructor() {
+    super();
+    //create lambda function which is added as listener later
+    this.handleValueChange = e => {
+      this.value = e.detail.data[this.refindex] * this.numerator / this.denominator;
+      if (this.value > this.thresholdvalue) {
+        if (!this.triggered) {
+          this.triggered = true;
+          this.trigger();
+        } else {
+          this.normalAction();
+        }
+      } else {
+        if (this.triggered) {this.triggered = false;}
+        this.normalAction();
+      }
+    };
+  }
+
+
+  attached() {
+    console.log('audioonincrease attached');
+    super.attached();
+    if (typeof this.thresholdvalue === 'string') {
+      this.thresholdvalue = parseFloat(this.thresholdvalue);
+    }
+  }
+
+  /**
+   * This should be overriden, is triggered when threshold is reached
+   */
+  trigger() {
+    console.log('trigger not implemented');
+    //not implemented
+  }
+
+  normalAction() {
+    //console.log('normalAction not implemented');
+    //usually do nothing, or some child may increase or implement some frame action
+  }
+}
