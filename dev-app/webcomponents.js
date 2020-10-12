f64722225fb7526044ced1677f94c7330edc604f
Tomas Kulhanek <tomaton@centrum.cz>
2020-10-12 08:34:49 +0100

    FIX markdown-it and katex, fix web-components dev-app for documentation

--- dev-app/webcomponents.js
+++ dev-app/webcomponents.js
@@ -1,13 +1,18 @@
+/**
+ * This dev-app/webcomponents shows how to register all plugin components as standadr WEB COMPONENTS.
+ * The forcePrefix=true sets prefix `bdl-` for all components, not only for that without hyphen `-`
+ * https://github.com/aurelia/web-components/pull/26
+ */
 import environment from './environment';
 import { CustomElementRegistry } from 'aurelia-web-components';
 import {PLATFORM} from 'aurelia-pal';
-
 
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
   //all components from src (resources/index) will be registered as web component
-    .feature(PLATFORM.moduleName('resources/index'));
+    .feature(PLATFORM.moduleName('resources/index'))
+    //.feature('resources/index');
   aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
   if (environment.testing) {
     aurelia.use.plugin('aurelia-testing');
@@ -16,7 +21,7 @@ export function configure(aurelia) {
   aurelia.start().then(() => {
     const registry = aurelia.container.get(CustomElementRegistry);
     registry.fallbackPrefix = 'bdl-';
-    registry.mandatoryPrefix = true;
+    registry.forcePrefix = true;
     registry.useGlobalElements();
   });
 }
