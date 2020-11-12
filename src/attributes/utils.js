dfaa22caf102d3752802f35110bb0ebd0d0adf81
Tomas Kulhanek <tomaton@centrum.cz>
2020-11-12 17:17:42 +0000

    UPDATE watch url alternative custom attribute

--- src/attributes/utils.js
+++ src/attributes/utils.js
@@ -8,11 +8,19 @@
  */
 export function parseHashParamString(hash) {
   //console.log('parseHashParamString',hash);
-  let paramsarray = hash.substr(1).split('&');
+  let paramsarray = hash.substr(1).split('&'); //gets all params seprated by &
   let params = {};
+  //go through all params
   for (let i = 0; i < paramsarray.length; i++) {
+    //gets param item - name value - if it is separated by = or only value if no '='
     let paramnamevalue = paramsarray[i].split('=', 2);
-    if (paramnamevalue.length === 1) {params[i] = paramnamevalue[0];} else {params[paramnamevalue[0]] = paramnamevalue[1];}
+    if (paramnamevalue.length === 1) {
+      //param is in form 'value' - so insert it as numbered param with index i
+      params[i] = paramnamevalue[0];
+    } else {
+      //param is in form 'name=value' - so insert as {name:value}
+      params[paramnamevalue[0]] = paramnamevalue[1];
+    }
   }
   return params;
 }
--- src/attributes/watchhashchange.js
+++ src/attributes/watchhashchange.js
@@ -1,21 +1,22 @@
-import {parseHashParamString} from './utils';
-import {WatchHashCore} from "./watch-hash-core";
+import {inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
 
 /**
  * Custom Attribute
  * if added to an element, cause that param name of param1 or index of param2 send in hash parameters are interpretted
  * to change SRC of parent element - this.elementVM.changesrc(newindex) is called
  */
-export class WatchhashCustomAttribute extends WatchHashCore {
-  static inject = [Element];
-  constructor(element) {
-    super();
+@inject(Element, EventAggregator)
+export class WatchhashchangeCustomAttribute {
+  //static inject = [Element];
+  constructor(element, ea) {
+    //super();
     this.element = element;
     this.isReadMDCustomElement = false;
     console.log('watchhash check au controller');
     if (element && element.au && element.au.controller) {
-      console.log('watchhash check au controller type:',typeof (element.au.controller));
-      if (typeof (element.au.controller) === 'object') console.log('watchhash check au controller viewmodel:',element.au.controller.viewModel);
+      console.log('watchhash check au controller type:', typeof (element.au.controller));
+      if (typeof (element.au.controller) === 'object') console.log('watchhash check au controller viewmodel:', element.au.controller.viewModel);
 
       if (typeof (element.au.controller) === 'object' && element.au.controller.viewModel && typeof (element.au.controller.viewModel) === 'object') {
         this.elementVM = element.au.controller.viewModel;
@@ -23,14 +24,20 @@ export class WatchhashCustomAttribute extends WatchHashCore {
         this.isReadMDCustomElement = (typeof this.elementVM.changesrc === 'function');
       }
     }
+    this.ea = ea;
+  }
+
+  attached() {
+    console.log('watchhash attribute attached from element', this.element);
   }
 
   bind() {
     this.params = this.value;
-    super.bind();
+    //super.bind();
+    this.ea.subscribe('hashchange', hashstruct =>{this.handleHash(hashstruct);});
   }
-  changesrc(...args) {
-    console.log('watchhash.changesrc()');
-    if (this.isReadMDCustomElement) this.elementVM.changesrc(...args);
+
+  handleHash(hashstruct) {
+    console.log('watchhash attribute handlehash()', hashstruct);
   }
 }
