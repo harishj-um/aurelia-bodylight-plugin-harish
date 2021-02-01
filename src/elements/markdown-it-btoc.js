26b0f75af9d323510beab3a9c128fc5a399205ac
Tomas Kulhanek <tomaton@centrum.cz>
2021-02-01 13:45:34 +0000

    UPDATE es6

--- src/elements/markdown-it-btoc.js
+++ src/elements/markdown-it-btoc.js
@@ -1,129 +1,128 @@
 //'use strict';
 
-var defaultOptions = {
-    tocRegexp: /@\[toc\]/im,
-    tocTitle: 'Table of Contents',
-    tocId: 'toc',
-    tocWrapperClass: 'toc',
-    tocLevelWrapperClass: 'toc-level',
-    anchorIdPrefix: 'h-',
-    reverseLink: false
+let defaultOptions = {
+  tocRegexp: /@\[toc\]/im,
+  tocTitle: 'Table of Contents',
+  tocId: 'toc',
+  tocWrapperClass: 'toc',
+  tocLevelWrapperClass: 'toc-level',
+  anchorIdPrefix: 'h-',
+  reverseLink: false
 };
 
 export function markdownitbtoc(md, options) {
+  // Set default options
+  var options = Object.assign({}, defaultOptions, options);
 
-    // Set default options
-    var options = Object.assign({}, defaultOptions, options);
+  // Global variables
+  if (!headingInfos) {
+    var headingInfos = [];
+  } else {
 
-    // Global variables
-    if (!headingInfos) {
-        var headingInfos = [];
-    } else {
+  }
 
+  md.inline.ruler.after('emphasis', 'toc', function(state, silent) {
+    if (silent) {
+      return false;
     }
-
-    md.inline.ruler.after('emphasis', 'toc', function (state, silent) {
-        if (silent) {
-            return false;
-        }
-        var match = options.tocRegexp.exec(state.src);
-        match = !match ? [] : match.filter(function (m) {
-            return m;
-        });
-        if (match.length < 1) {
-            return false;
-        }
-
-        var token;
-
-        token = state.push("toc_open", "toc", 1);
-        token.markup = match[0];
-        token = state.push("toc_body", "", 0);
-        token = state.push("toc_close", "toc", -1);
-
-        // to continue pasing, Update pos
-        state.pos = state.pos + match[0].length;
-        return true;
+    let match = options.tocRegexp.exec(state.src);
+    match = !match ? [] : match.filter(function(m) {
+      return m;
     });
-
-    md.core.ruler.push('init_toc', function (state) {
-
-        // For each rendering, initialize heading count
-        var headingCounts = [null, 0, 0, 0, 0, 0, 0];
-        var tokens = state.tokens;
-
-        // Parses all heading information to render the TOC
-        for (var i = 0; i < tokens.length; i++)
-            if (tokens[i].type === 'heading_open') {
-                var tagLevel = parseInt(tokens[i].tag[1]);
-                var numbering = [];
-
-                headingCounts[tagLevel] += 1;
-
-                for (var j = 1; j < headingCounts.length; j++) {
-                    if (j <= tagLevel) {
-                        numbering.push(headingCounts[j]);
-                    } else {
-                        headingCounts[j] = 0;
-                    }
-                }
-
-                var hInfo = {
-                    numbering: numbering,
-                    content: tokens[i + 1].content
-                };
-
-                headingInfos.push(hInfo);
-            }
-    });
-
-    md.renderer.rules.toc_open = function (tokens, index) {
-        return '<div id="' + options.tocId + '" class="' + options.tocWrapperClass + '">';
+    if (match.length < 1) {
+      return false;
     }
 
-    md.renderer.rules.toc_close = function (token, index) {
-        return '</div>';
-    }
+    let token;
 
-    md.renderer.rules.toc_body = function (tokens, index) {
-        var results = [];
-        var previousLevel = 0;
+    token = state.push('toc_open', 'toc', 1);
+    token.markup = match[0];
+    token = state.push('toc_body', '', 0);
+    token = state.push('toc_close', 'toc', -1);
 
-        for (var i = 0; i < headingInfos.length; i++) {
-            var hInfo = headingInfos[i];
-            var levelDiff = hInfo.numbering.length - previousLevel;
+    // to continue pasing, Update pos
+    state.pos = state.pos + match[0].length;
+    return true;
+  });
 
-            if (levelDiff > 0) {
-                for (var _ = 0; _ < levelDiff; _++) {
-                    results.push('<ul class="' + options.tocLevelWrapperClass + '">');
-                }
-            } else if (levelDiff < 0) {
-                for (var _ = 0; _ > levelDiff; _--) {
-                    results.push('</ul>');
-                }
-            }
+  md.core.ruler.push('init_toc', function(state) {
+    // For each rendering, initialize heading count
+    let headingCounts = [null, 0, 0, 0, 0, 0, 0];
+    let tokens = state.tokens;
 
-            var numberingStr = hInfo.numbering.join('.');
-            var anchor = options.anchorIdPrefix + numberingStr;
+    // Parses all heading information to render the TOC
+    for (let i = 0; i < tokens.length; i++) {
+      if (tokens[i].type === 'heading_open') {
+        let tagLevel = parseInt(tokens[i].tag[1]);
+        let numbering = [];
 
-            results.push('<li> <a onclick="document.getElementById(\'' + anchor + '\').scrollIntoView();">' + numberingStr + '. ' + hInfo.content + '</a></li>');
+        headingCounts[tagLevel] += 1;
 
-            previousLevel = headingInfos[i].numbering.length;
+        for (let j = 1; j < headingCounts.length; j++) {
+          if (j <= tagLevel) {
+            numbering.push(headingCounts[j]);
+          } else {
+            headingCounts[j] = 0;
+          }
         }
 
-        // Add the remaining </ul> tags at end of TOC.
-        for (var i = 0; i < previousLevel; i++) {
-            results.push('</ul>');
+        let hInfo = {
+          numbering: numbering,
+          content: tokens[i + 1].content
+        };
+
+        headingInfos.push(hInfo);
+      }
+    }
+  });
+
+  md.renderer.rules.toc_open = function(tokens, index) {
+    return '<div id="' + options.tocId + '" class="' + options.tocWrapperClass + '">';
+  };
+
+  md.renderer.rules.toc_close = function(token, index) {
+    return '</div>';
+  };
+
+  md.renderer.rules.toc_body = function(tokens, index) {
+    let results = [];
+    let previousLevel = 0;
+
+    for (let i = 0; i < headingInfos.length; i++) {
+      let hInfo = headingInfos[i];
+      let levelDiff = hInfo.numbering.length - previousLevel;
+
+      if (levelDiff > 0) {
+        for (let _ = 0; _ < levelDiff; _++) {
+          results.push('<ul class="' + options.tocLevelWrapperClass + '">');
         }
+      } else if (levelDiff < 0) {
+        for (let _ = 0; _ > levelDiff; _--) {
+          results.push('</ul>');
+        }
+      }
 
-        return results.join('');
+      let numberingStr = hInfo.numbering.join('.');
+      let anchor = options.anchorIdPrefix + numberingStr;
+
+      results.push('<li> <a onclick="document.getElementById(\'' + anchor + '\').scrollIntoView();">' + numberingStr + '. ' + hInfo.content + '</a></li>');
+
+      previousLevel = headingInfos[i].numbering.length;
     }
 
-    md.renderer.rules.heading_open = function (tokens, index) {
-        var hInfo = headingInfos.shift();
-        var numberingStr = hInfo.numbering.join('.');
-        var anchor = options.anchorIdPrefix + numberingStr;
-
-        return '<' + tokens[index].tag + '><a id="' + anchor + '">' + numberingStr + '.</a> ';
+    // Add the remaining </ul> tags at end of TOC.
+    for (let i = 0; i < previousLevel; i++) {
+      results.push('</ul>');
     }
+
+    return results.join('');
+  };
+
+  md.renderer.rules.heading_open = function(tokens, index) {
+    let hInfo = headingInfos.shift();
+    let numberingStr = hInfo.numbering.join('.');
+    let anchor = options.anchorIdPrefix + numberingStr;
+
+    return '<' + tokens[index].tag + '><a id="' + anchor + '">' + numberingStr + '.</a> ';
+  };
 }
