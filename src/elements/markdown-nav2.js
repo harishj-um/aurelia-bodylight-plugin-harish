dffaef18ceb439588310f3f2988eb574ad2a1d99
Tomas Kulhanek <tomaton@centrum.cz>
2021-02-02 11:00:45 +0000

    UPDATE eslint

--- src/elements/markdown-nav2.js
+++ src/elements/markdown-nav2.js
@@ -3,7 +3,7 @@ import {I18N} from 'aurelia-i18n';
 import {HttpClient} from 'aurelia-fetch-client';
 import {Markdownnav} from './markdownnav';
 import {EventAggregator} from 'aurelia-event-aggregator';
-import {useView} from "aurelia-templating";
+import {useView} from 'aurelia-templating';
 
 @inject(I18N, HttpClient, EventAggregator)
 @useView('./markdownnav.html')
