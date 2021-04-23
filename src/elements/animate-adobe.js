dc3c5da75dab10eefd1d11749395e64d02637252
Tomas Kulhanek <tomaton@centrum.cz>
2021-04-23 12:29:57 +0100

    UPDATE eslint

--- src/elements/animate-adobe.js
+++ src/elements/animate-adobe.js
@@ -378,6 +378,8 @@ export class AnimateAdobe {
       if (!bindings) return;
       for (let binding of bindings) {
         //binding = {findex:findex,aname:aname,amin:amin,amax:amax,fmin:fmin,fmax:fmax}
+        //it might be tring or number - from custom element attribute
+        // eslint-disable-next-line eqeqeq
         let value = (binding.findex == -1) ? e.detail.time : e.detail.data[binding.findex];
         if (binding.aname.includes('_text')) {
           let convertedvalue = binding.convertf2a(value); //precision
