be9620a6b8cebd88abd7aa301596f6886501f037
Tomas Kulhanek <tomaton@centrum.cz>
2021-11-26 17:40:23 +0100

    UPDATE pdbe-molstar 1.2 and chartjs-fixed

--- src/elements/chartjs-fixed-xy.js
+++ src/elements/chartjs-fixed-xy.js
@@ -1,15 +1,17 @@
 import {bindable} from 'aurelia-templating';
 import {ChartjsFixed} from './chartjs-fixed';
 import {useView} from 'aurelia-templating';
-
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import Chart from 'chart.js';
 
 /**
  * shows fixed curve at time -
- * on X is 0,1,2,3,4,5,6,7,8,9
+ * on X isvalues from FMU variables from xrefindex to xrefvalues
  * on Y is values from FMU variables from refindex to refvalues
  * convertors for x and y axis separated by ;
  * refindex, refvalues for y values
  * xrefindex,xrefvalues for x values
+ * refpointindex
  */
 @useView('./chartjs.html')
 export class ChartjsFixedXy extends ChartjsFixed {
@@ -23,7 +25,9 @@ export class ChartjsFixedXy extends ChartjsFixed {
     @bindable maxdata=3;
     @bindable xrefindex;
     @bindable xrefvalues;
-    @bindable xtofixed = -1;
+    @bindable xtofixed = 0;
+    @bindable refpointindex;
+    @bindable xrefpointindex;
 
     //@bindable cachesize;
     currentdataset=0;
@@ -34,19 +38,34 @@ export class ChartjsFixedXy extends ChartjsFixed {
             //all values from refindex to one dataset - as one curve
             let ydata = e.detail.data.slice(this.refindex, this.refindex + this.refvalues)
             let xdata = e.detail.data.slice(this.xrefindex, this.xrefindex + this.xrefvalues)
-            //
+            let xpoint = 0;
+            let ypoint= 0;
+            //point to highlight
+            if (this.refpointindex) {
+                xpoint = e.detail.data[this.xrefpointindex];
+                ypoint = e.detail.data[this.refpointindex];
+            }
+            //convertors
+            if (this.operation && this.operation[0] && this.operation[1]){
+                xdata = xdata.map(x => this.operation[0](x));
+                ydata = ydata.map(y => this.operation[1](y));
+                if (this.refpointindex) {
+                    xpoint = this.operation[0](xpoint);
+                    ypoint = this.operation[1](ypoint);
+                }
+            }
             let data = [];
             for (let i = 0; i < ydata.length; i++) {
-                if (this.operation && this.operation[0] && this.operation[1])
-                    data.push({x: this.operation[0](xdata[i]), y: this.operation[1](ydata[i])});
-                else data.push({x: xdata[i], y: ydata[i]});
+                data.push({x: xdata[i], y: ydata[i]});
             }
+            let data2 = [{x:xpoint,y:ypoint}];
+
             //set labels to x axis
             if (this.xtofixed >= 0) this.chart.data.labels = xdata.map(x => x.toFixed(this.xtofixed));
 
             //set data xy to chart struct
             //do initialize dataset first
-            this.chart.data.datasets.unshift({
+            let newdataset = {
                 data: data,
                 label: "",
                 backgroundColor: this.currentcolor,
@@ -54,14 +73,30 @@ export class ChartjsFixedXy extends ChartjsFixed {
                 borderWidth: 1,
                 pointRadius: 1,
                 fill: false
-            });
-            if (this.chart.data.datasets[1]) {
-                this.chart.data.datasets[1].backgroundColor = this.previouscolor;
-                this.chart.data.datasets[1].borderColor = this.previouscolor;
+            };
+            let colorindex = 1;
+            if (this.refpointindex) {
+                let newpointdataset = {
+                    data: data2,
+                    label: "",
+                    backgroundColor: this.currentcolor,
+                    borderColor: this.currentcolor,
+                    borderWidth: 1,
+                    pointRadius: 2,
+                    fill: false
+                }
+                this.chart.data.datasets[0] = newdataset;
+                this.chart.data.datasets.unshift(newpointdataset);
+            } else {
+                this.chart.data.datasets.unshift(newdataset);
             }
-            if (this.chart.data.datasets[2]) {
-                this.chart.data.datasets[2].backgroundColor = this.previouscolor2;
-                this.chart.data.datasets[2].borderColor = this.previouscolor2;
+            if (this.chart.data.datasets[colorindex]) {
+                this.chart.data.datasets[colorindex].backgroundColor = this.previouscolor;
+                this.chart.data.datasets[colorindex].borderColor = this.previouscolor;
+            }
+            if (this.chart.data.datasets[colorindex+1]) {
+                this.chart.data.datasets[colorindex+1].backgroundColor = this.previouscolor2;
+                this.chart.data.datasets[colorindex+1].borderColor = this.previouscolor2;
             }
                 if (this.chart.data.datasets.length > this.maxdata) this.chart.data.datasets.pop();
                 this.updatechart();
@@ -71,10 +106,12 @@ export class ChartjsFixedXy extends ChartjsFixed {
 
     bind(){
         super.bind();
+        this.data.labels = [];
         this.xrefindex = parseInt(this.xrefindex, 10);
         if (!this.xrefindex) console.warn('xrefindex is not specified');
         this.xrefvalues = parseInt(this.xrefvalues, 10);
-        if (this.xrefvalues !== this.refvalues) console.warn('xrefvalues must be equal to refvalues');
+        if (this.xrefvalues !== this.refvalues) console.warn('the value of "xrefvalues" must be equal to "refvalues"');
+        this.xrefpointindex = parseInt(this.xrefpointindex, 10);
     }
 
     attached() {
--- src/elements/chartjs-fixed.js
+++ src/elements/chartjs-fixed.js
@@ -2,6 +2,7 @@ import {bindable} from 'aurelia-templating';
 import {Chartjs} from './chartjs';
 import {useView} from 'aurelia-templating';
 import _ from 'lodash';
+import Chart from 'chart.js';
 
 /**
  * shows fixed curve at time -
@@ -10,15 +11,17 @@ import _ from 'lodash';
  */
 @useView('./chartjs.html')
 export class ChartjsFixed extends Chartjs {
-    @bindable fromid;
-    @bindable labels;
-    @bindable refindex;
-    @bindable refvalues;
-    @bindable type;
-    @bindable min;
-    @bindable max;
-    @bindable maxdata=3;
-    @bindable colorindex=0;
+    @bindable fromid; //DOM id of FMU component to listen fmu-data event
+    @bindable labels; //labels of datasets
+    @bindable refindex; //variable index in fmu data
+    @bindable refvalues; //how many variables to show from fmu data
+    @bindable type; //type of chart - is overwritten to 'line'
+    @bindable min; //if defined chartjs y axis from min
+    @bindable max; //if defined chartjs y axis to max
+    @bindable maxdata=3; //howmany datasets to remember
+    @bindable colorindex=0; //from whic colorindex to start, 0 blue, 1 red, 2 green,...
+    @bindable highlightindex;
+    @bindable refpointindex; //extra dataset with only one point is drawn
     currentcolor;
     previouscolor;
     previouscolor2;
@@ -32,26 +35,54 @@ export class ChartjsFixed extends Chartjs {
             //all values from refindex to one dataset - as one curve
             //if (!this.chart.data.datasets[j]) {
                 //do initialize dataset first
-            this.chart.data.datasets.unshift({
-                    data: e.detail.data.slice(this.refindex,this.refindex+this.refvalues),
+            let newdataset = {
+                data: e.detail.data.slice(this.refindex,this.refindex+this.refvalues),
+                label:"",
+                backgroundColor: this.currentcolor,
+                borderColor: this.currentcolor,
+                borderWidth: 1,
+                pointRadius: 1,
+                fill: false
+            }
+            //index to blure color
+            let colorindex=1;
+            //decide whether to add point
+            if (this.refpointindex) {
+                let newpointdataset = {
+                    data: [e.detail.data[this.refpointindex]],
                     label:"",
                     backgroundColor: this.currentcolor,
                     borderColor: this.currentcolor,
                     borderWidth: 1,
-                    pointRadius: 1,
+                    pointRadius: 0,
                     fill: false
-                })
-            if(this.chart.data.datasets[1]) {
-                this.chart.data.datasets[1].backgroundColor = this.previouscolor;
-                this.chart.data.datasets[1].borderColor = this.previouscolor;
+                }
+                this.chart.data.datasets[0] = newdataset;
+                this.chart.data.datasets.unshift(newpointdataset)
+                //blur color from inde 2, [0] is point [1] is dataset
+                colorindex = 2;
+            } else {
+                this.chart.data.datasets.unshift(newdataset);
             }
-            if(this.chart.data.datasets[2]) {
-                this.chart.data.datasets[2].backgroundColor = this.previouscolor2;
-                this.chart.data.datasets[2].borderColor = this.previouscolor2;
+            if(this.chart.data.datasets[colorindex]) {
+                this.chart.data.datasets[colorindex].backgroundColor = this.previouscolor;
+                this.chart.data.datasets[colorindex].borderColor = this.previouscolor;
+            }
+            if(this.chart.data.datasets[colorindex+1]) {
+                this.chart.data.datasets[colorindex+1].backgroundColor = this.previouscolor2;
+                this.chart.data.datasets[colorindex+1].borderColor = this.previouscolor2;
             }
             //do apply operation on each element of array
-            if (this.operation && this.operation[0])
-                this.chart.data.datasets[0].data.map(item => {return this.operation[0](item)});
+            if (this.operation && this.operation[0]) {
+                this.chart.data.datasets[0].data.map(item => {
+                    return this.operation[0](item)
+                });
+                if (this.refpointindex) { //do conversion on [1] too
+                    this.chart.data.datasets[1].data.map(item => {
+                        return this.operation[0](item)
+                    });
+                }
+            }
 
             if (this.chart.data.datasets.length>this.maxdata) { this.chart.data.datasets.pop();}
 
@@ -83,10 +114,64 @@ export class ChartjsFixed extends Chartjs {
         this.currentcolor =  this.selectColor(this.colorindex,65);
         this.previouscolor = this.selectColor(this.colorindex,65,75);
         this.previouscolor2 = this.selectColor(this.colorindex,65,95);
+        this.refpointindex = parseInt(this.refpointindex, 10);
+        if (this.refpointindex) {
+            this.options.refpointplugin = {index:this.refpointindex-this.refindex};
+        }
     }
 
     attached() {
+        if (this.refpointindex) {
+            Chart.pluginService.register({
+                id: 'custom_lines to ref point',
+                afterDraw: (chart) => {
+                    if (chart.config.options.refpointplugin) {
+                        const ctx = chart.canvas.getContext('2d');
+                        ctx.save();
+
+                        // draw line
+                        let meta1 = chart.getDatasetMeta(0);
+                        //let meta2 = chart.getDatasetMeta(1);
+                        if (meta1) {
+                            ctx.beginPath();
+                            try {
+                                //expect that data[0] contains point data[1] lines
+                                let x = meta1.data[0]._model.x;
+                                let y = meta1.data[0]._model.y;
+                                let value = chart.data.datasets[0].data[0];
+                                if (isNaN(value)) {
+                                    //value is object x, y
+                                    ctx.moveTo(0, y);
+                                    ctx.lineTo(x, y);
+                                    ctx.lineTo(x, chart.height);
+                                    ctx.lineWidth = 1;
+                                    ctx.strokeStyle = '#ff9c9c';
+                                    ctx.stroke();
+                                    ctx.font = "10px Arial"
+                                    if (value.y) ctx.fillText(value.y.toPrecision(4),x,y);
+                                } else {
+                                    //only y value is there, draw line
+                                    ctx.moveTo(0, y);
+                                    ctx.lineTo(chart.width, y);
+                                    ctx.lineWidth = 1;
+                                    ctx.strokeStyle = '#ff9c9c';
+                                    ctx.stroke();
+                                    ctx.font = "10px Arial"
+                                    ctx.fillStyle = "black";
+                                    ctx.fillText(value.toPrecision(4),x,y);
+                                }
+                            } catch (e) {
+                                //console.warn('error, meta1:',meta1);
+                            }
+                            ctx.restore();
+                        }
+
+                    }
+                }
+            })
+        }
         super.attached();
+        if (this.refpointindex) console.log('chartjs fixed debug: chart:',this.chart);
     }
 
 }
