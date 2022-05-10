8e0a28ea9d75b6e0f2ae13eb25fae672897c2961
Tomas Kulhanek <tomaton@centrum.cz>
2022-05-10 09:18:12 +0200

    UPDATE remotevalue periodic update and GET POST request options

--- docs/example/remotevaluedemo.md
+++ docs/example/remotevaluedemo.md
@@ -3,7 +3,9 @@
 ```
 <bdl-remote-value></bdl-remote-value>
 ```
-<bdl-remote-value></bdl-remote-value>
+<bdl-remote-value manequinurl="http://localhost:5000/" interval="500" id="id4"></bdl-remote-value>
+<bdl-chartjs-time id="id11" width="400" height="300" fromid="id4" refindex="7" refvalues="1" ></bdl-chartjs-time>
+<bdl-chartjs-time id="id11" width="400" height="300" fromid="id4" refindex="0" refvalues="6" ></bdl-chartjs-time>
 
 </div>
 
--- src/elements/remote-value.js
+++ src/elements/remote-value.js
@@ -5,11 +5,14 @@ import {inject,bindable} from "aurelia-framework";
 export class RemoteValue {
     value;
     @bindable remoteurl;
+    @bindable started = false;
     remotevalue;
     @bindable remoteheader='x-api-key';
     @bindable remoteheadervalue='';
     postvalue='';
-    @bindable fetchinterval = 0;
+    @bindable interval = 0;
+    @bindable id;
+    starttime;
 
     constructor(httpclient) {
         this.client = httpclient;
@@ -21,25 +24,40 @@ export class RemoteValue {
         }
     }
 
+    bind(){
+        if (this.id) {
+            //will generate fmidata event
+        }
+        if (typeof this.started === 'string') {
+            this.started = this.started === 'true';
+        }
+    }
+
     attached() {
+        this.time = new Date();
+        this.starttime = Math.round(this.time.getTime() /1000);
         this.remoteurl =         localStorage.getItem('bdl-fhir-url');
         this.remoteheadervalue = localStorage.getItem('bdl-fhir-api-key');
-        if (typeof(getinterval) === 'string'){
-            this.fetchinterval = parseInt(this.fetchinterval)
+        if (typeof(interval) === 'string'){
+            this.interval = parseInt(this.interval)
 
         }
     }
 
-    fetchintervalChange(newValue,oldValue) {
+    intervalChanged(newValue,oldValue) {
         //triggered by aurelia fw when getinterval is changed
-        if (typeof(this.fetchinterval) === 'string'){
-            this.fetchinterval = parseInt(this.fetchinterval)
+        if (typeof(this.interval) === 'string'){
+            this.interval = parseInt(this.interval)
         }
     }
 
     start(){
-        this.get();
-        if (this.fetchinterval>0) setTimeout(this.handleTick.bind(this),this.fetchinterval);
+        //this.get();
+        this.started = ! this.started;
+        if (this.started) {
+            this.fetchinterval = this.interval;
+            setTimeout(this.handleTick.bind(this), this.fetchinterval);
+        } else  {this.fetchinterval = 0;}
     }
 
     get() {
@@ -56,8 +74,19 @@ export class RemoteValue {
                 //console.log('markdownaurelia fetched md:', data)
                 this.remotevalue = data;
                 this.remotevalueformatted = JSON.stringify(this.remotevalue,null,4)
+                if (this.id) {
+                    //generatefmidata event
+                    let mydata =[];
+                    let mytime = (Math.round((new Date()).getTime() /100) - (this.starttime*10))/10;
+                    if (typeof this.remotevalue === 'object') {
+                        for (let key of Object.keys(this.remotevalue)) mydata.push(this.remotevalue[key]);
+                    }
+                    else if (typeof this.remotevalue === 'number') {mydata.push(this.remotevalue)}
+                    let event = new CustomEvent('fmidata', {detail: {time: mytime, data: mydata}});
+                    document.getElementById(this.id).dispatchEvent(event);
+                }
             })
-            .cath(err =>{console.log('error',error);this.fetchinterval=0;});
+            .catch(err =>{console.log('error',err);this.fetchinterval=0;});
         /*this.client.get(this.remoteurl)
             .then(response => response.json())// do response.json() for json result
             .then(data => {
@@ -66,6 +95,13 @@ export class RemoteValue {
                 this.remotevalueformatted = JSON.stringify(this.remotevalue,null,4)
             });*/
     }
+
+    round(value, decimals) {
+        if (decimals < 0) {let posdecimals = -decimals; return Number(Math.round(value + 'e' + posdecimals) + 'e-' + posdecimals);}
+        return Number(Math.round(value + 'e-' + decimals) + 'e+' + decimals);
+    }
+
+
     post() {
         //sends GET request to
         let myheaders = new Headers();
