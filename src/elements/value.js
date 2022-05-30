703f32a4c921fc8b318448724d3f58cba3919cf2
Tomas Kulhanek <tomaton@centrum.cz>
2022-05-30 14:00:55 +0200

    UPDATE remotevalue

--- src/elements/value.js
+++ src/elements/value.js
@@ -57,7 +57,7 @@ export class Value {
           else {
             let numerator = parseFloat(convertitems[0]);
             let denominator = parseFloat(convertitems[1]);
-            let addend = (convertitems.length>1)?parseFloat(convertitems[2]):0;
+            let addend = (convertitems.length>2)?parseFloat(convertitems[2]):0;
             this.operation.push(x => ((x * numerator / denominator) + addend));
           }
         } else {
